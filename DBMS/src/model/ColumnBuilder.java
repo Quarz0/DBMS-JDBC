7e5b4652cbac4178765747616efb93514c8d7779
Ahmed Magdy <ahmed.magdy.g.s@gmail.com>
2016-11-25 00:50:06 +0200

    column builder

---
+++ DBMS/src/model/ColumnBuilder.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.util.List;
+
+public class ColumnBuilder {
+    
+    public static <cls> Column buildColumn(Class<?> cls, String colName) {
+        return new Column<cls>(colName);
+    }
+    
+}
--- DBMS/src/model/Table.java
+++ DBMS/src/model/Table.java
@@ -5,11 +5,12 @@ import java.util.List;
 public class Table {
     private List<Column<?>> columnsList;
     private List<String> header;
+    private ColumnBuilder columnBuilder;
 
     public Table(List<String> colName, List<Class<?>> types) {
         header = colName;
-        for (int i = 0; i < values.size(); i++) {
-            Column<types.get(i).class> temp = new Column<>(colName.get(i));
+        for (int i = 0; i < types.size(); i++) {
+            Column temp = columnBuilder.buildColumn(types.get(i), colName.get(i));
             columnsList.add(temp);
         }
     }
