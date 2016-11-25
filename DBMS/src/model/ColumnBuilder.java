0353c03f4d6d64866d61d7ec25426e4d5e6c9a94
mahmoud <mahmoud@Mahmoud-PC>
2016-11-25 14:23:04 +0200

    ColumnBuilder fixed warning and tested.

--- DBMS/src/model/ColumnBuilder.java
+++ DBMS/src/model/ColumnBuilder.java
@@ -1,10 +1,8 @@
 package model;
 
-import java.util.List;
-
 public class ColumnBuilder {
 
-    public static <cls> Column buildColumn(Class<?> cls, String colName) {
+    public static <cls> Column<cls> buildColumn(Class<?> cls, String colName) {
         return new Column<cls>(colName, cls);
     }
 
--- DBMS/src/tester/Test.java
+++ DBMS/src/tester/Test.java
@@ -1,9 +1,43 @@
 package tester;
 
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Table;
+
 public class Test {
 
     @org.junit.Test
     public void test() {
+        List<String> colNames = new ArrayList<>();
+        colNames.add("0");
+        colNames.add("1");
+        colNames.add("2");
+        colNames.add("3");
+        colNames.add("4");
+        colNames.add("5");
+        colNames.add("6");
+        colNames.add("7");
+        List<Class<?>> colTypes = new ArrayList<>();
+        colTypes.add(Integer.class);
+        colTypes.add(String.class);
+        colTypes.add(Double.class);
+        colTypes.add(Float.class);
+        colTypes.add(Short.class);
+        colTypes.add(Byte.class);
+        colTypes.add(Character.class);
+        colTypes.add(Long.class);
+        Table testTable = new Table("testTable", "noDirec", colNames, colTypes);
+        assertEquals(Integer.class, testTable.getColumn(0).getType());
+        assertEquals(String.class, testTable.getColumn(1).getType());
+        assertEquals(Double.class, testTable.getColumn(2).getType());
+        assertEquals(Float.class, testTable.getColumn(3).getType());
+        assertEquals(Short.class, testTable.getColumn(4).getType());
+        assertEquals(Byte.class, testTable.getColumn(5).getType());
+        assertEquals(Character.class, testTable.getColumn(6).getType());
+        assertEquals(Long.class, testTable.getColumn(7).getType());
     }
 
 }
