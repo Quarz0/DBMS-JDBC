b762a02915da3d5ed58a73b92ce38111d955fa7e
Hesham El-Sawaf <heshamlsawaf@gmail.com>
2016-11-27 02:49:53 +0200

    Implemented Insert class

--- DBMS/src/model/statements/Insert.java
+++ DBMS/src/model/statements/Insert.java
@@ -1,11 +1,93 @@
 package model.statements;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Pair;
+import util.App;
+import util.Regex;
+import util.RegexEvaluator;
+
 public class Insert implements Query {
+
+    private String tableIdentifier;
+    private List<Pair<String, String>> columns;
+    private boolean isDefaultSelection;
+
+    public Insert() {
+        this.columns = new ArrayList<>();
+        this.isDefaultSelection = false;
+    }
 
     @Override
     public void parse(String s) {
-        // TODO Auto-generated method stub
+        if (!App.checkForExistence(s) || !this.checkRegex(s))
+            this.callForFailure();
+    }
 
+    private boolean checkRegex(String s) {
+        String[] groups = RegexEvaluator.evaluate(s, Regex.PARSE_WITH_INSERT);
+        if (App.checkForExistence(groups)) {
+            this.extractTable(groups[1].trim());
+            String[] values = this.extractValues(groups[3].trim());
+            if (!App.checkForExistence(groups[2])) {
+                this.isDefaultSelection = true;
+                this.fillColumns(values, null);
+            } else {
+                this.isDefaultSelection = false;
+                String[] identifiers = this.extractIdentifiers(groups[2].trim());
+                this.fillColumns(values, identifiers);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    private void fillColumns(String[] values, String[] identifiers) {
+        Pair<String, String> pair;
+        if (App.checkForExistence(identifiers) && values.length != identifiers.length)
+            this.callForFailure();
+        for (int i = 0; i < values.length; i++) {
+            if (App.checkForExistence(identifiers))
+                pair = new Pair<String, String>(values[i], identifiers[i]);
+            else
+                pair = new Pair<String, String>(values[i], null);
+            this.columns.add(pair);
+        }
+    }
+
+    private String[] extractValues(String s) {
+        if (!s.matches(Regex.PARSE_WITH_INSERT_TRIM_MATCH))
+            this.callForFailure();
+        if (s.split(Regex.PARSE_WITH_INSERT_SPLIT_PATTERN1).length <= s
+                .split(Regex.PARSE_WITH_INSERT_SPLIT_PATTERN2).length)
+            return s.split(Regex.PARSE_WITH_INSERT_SPLIT_PATTERN1);
+        else
+            return s.split(Regex.PARSE_WITH_INSERT_SPLIT_PATTERN2);
+    }
+
+    private String[] extractIdentifiers(String s) {
+        return s.split(",");
+    }
+
+    private void extractTable(String s) {
+        this.tableIdentifier = s.trim();
+    }
+
+    private void callForFailure(/* Exception e */) {
+
+    }
+
+    public String getTableIdentifier() {
+        return this.tableIdentifier;
+    }
+
+    public List<Pair<String, String>> getColumns() {
+        return columns;
+    }
+
+    public boolean isDefaultSelection() {
+        return isDefaultSelection;
     }
 
 }
--- DBMS/src/util/Regex.java
+++ DBMS/src/util/Regex.java
@@ -14,6 +14,11 @@ public final class Regex {
     public static final String PARSE_WITH_DROP_DATABASE = "\\A\\s*DATABASE\\s+([a-zA-Z_]\\w*)\\s*\\Z";
     public static final String PARSE_WITH_DROP_TABLE = "\\A\\s*TABLE\\s+([a-zA-Z_]\\w*)\\s*\\Z";
 
+    public static final String PARSE_WITH_INSERT = "\\A\\s*INTO\\s+([a-zA-Z_]\\w*)(?:\\(\\s*([a-zA-Z_]\\w*\\s*(?:,\\s*[a-zA-Z_]\\w*\\s*)*)\\))?\\s+VALUES\\s*\\((.+)\\)\\s*\\Z";
+    public static final String PARSE_WITH_INSERT_TRIM_MATCH = ".+(?<!,)";
+    public static final String PARSE_WITH_INSERT_SPLIT_PATTERN1 = ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)";
+    public static final String PARSE_WITH_INSERT_SPLIT_PATTERN2 = ",(?=(?:[^\']*\'[^\']*\')*[^\']*$)";
+
     private Regex() {
     }
 }
