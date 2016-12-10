43b02dea79fba32168120698b5c595140db952ef
Marwan Tammam <darknight-x@hotmail.com>
2016-12-10 22:25:05 +0200

    jdbc classes

---
+++ DBMS/src/plugins/jdbc/Statement.java
@@ -0,0 +1,249 @@
+package plugins.jdbc;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.SQLWarning;
+import java.util.ArrayList;
+import java.util.List;
+
+import util.App;
+
+public class Statement implements java.sql.Statement {
+
+    private List<String> commands;
+
+    @Override
+    public void addBatch(String sql) throws SQLException {
+        if (!App.checkForExistence(this.commands)) {
+            this.commands = new ArrayList<>();
+        }
+        this.commands.add(sql);
+    }
+
+    @Override
+    public void clearBatch() throws SQLException {
+        if (App.checkForExistence(this.commands)) {
+            this.commands.clear();
+        }
+    }
+
+    @Override
+    public void close() throws SQLException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public boolean execute(String sql) throws SQLException {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public int[] executeBatch() throws SQLException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ResultSet executeQuery(String arg0) throws SQLException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int executeUpdate(String arg0) throws SQLException {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public Connection getConnection() throws SQLException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ResultSet getResultSet() throws SQLException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int getUpdateCount() throws SQLException {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getQueryTimeout() throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setQueryTimeout(int arg0) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+    
+    @Override
+    public boolean execute(String arg0, int arg1) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean execute(String arg0, int[] arg1) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean execute(String arg0, String[] arg1) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int executeUpdate(String arg0, int arg1) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int executeUpdate(String arg0, int[] arg1) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int executeUpdate(String arg0, String[] arg1) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean isWrapperFor(Class<?> arg0) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public <T> T unwrap(Class<T> arg0) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void cancel() throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void clearWarnings() throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void closeOnCompletion() throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int getFetchDirection() throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int getFetchSize() throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public ResultSet getGeneratedKeys() throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int getMaxFieldSize() throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int getMaxRows() throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean getMoreResults() throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean getMoreResults(int arg0) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int getResultSetConcurrency() throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int getResultSetHoldability() throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int getResultSetType() throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public SQLWarning getWarnings() throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean isCloseOnCompletion() throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean isClosed() throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean isPoolable() throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setCursorName(String arg0) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setEscapeProcessing(boolean arg0) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setFetchDirection(int arg0) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setFetchSize(int arg0) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setMaxFieldSize(int arg0) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setMaxRows(int arg0) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setPoolable(boolean arg0) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+}
