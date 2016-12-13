99fd9d8377071f46dbdde44380770379807cf853
Marwan Tammam <darknight-x@hotmail.com>
2016-12-13 17:19:19 +0200

    fix in order and evaluate

--- DBMS/src/controller/ClauseController.java
+++ DBMS/src/controller/ClauseController.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
 import javax.script.ScriptException;
 
@@ -51,6 +52,11 @@ public class ClauseController implements DBMSClause {
         exp = App.replace(exp, "and", " && ");
         exp = App.replace(exp, "or", " || ");
         exp = App.replace(exp, "not", " ! ");
+        exp = App.replace(exp, "=", "==");
+        exp = App.replace(exp, ">==", ">=");
+        exp = App.replace(exp, "<==", "<=");
+        exp = App.replace(exp, "====", "==");
+
         try {
             return BooleanEvaluator.evaluate(exp);
         } catch (ScriptException e) {
@@ -63,8 +69,7 @@ public class ClauseController implements DBMSClause {
         int i = 0;
         for (String column : record.getColumns().keySet()) {
             if (record.getColumns().get(column).equals(String.class)) {
-                exp = App.replace(exp, column.toLowerCase(),
-                        "\"" + record.getValues().get(i).toString() + "\"");
+                exp = App.replace(exp, column.toLowerCase(), record.getValues().get(i).toString());
             } else {
                 exp = App.replace(exp, column.toLowerCase(), record.getValues().get(i).toString());
             }
@@ -75,7 +80,7 @@ public class ClauseController implements DBMSClause {
 
     @Override
     public void order(Map<String, String> columns) throws RuntimeException {
-        Map<String, Integer> columnIndex = new HashMap<>();
+        Map<String, Integer> columnIndex = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
         SelectionTable table = this.dbmsController.getDatabaseController().getHelper()
                 .getSelectedTable();
         int i = 0;
@@ -90,7 +95,7 @@ public class ClauseController implements DBMSClause {
                         CompareToBuilder compare = new CompareToBuilder();
                         for (String column : columns.keySet()) {
                             int index = columnIndex.get(column);
-                            Class<?> type = r1.getColumns().get(column);
+                            Class<?> type = table.getDefaultHeader().get(column.toLowerCase());
                             if (columns.get(column).equals("ASC"))
                                 compare.append(
                                         TypeFactory.parseToObject(type,
