88afb1e7214b2ba7fe83e0485aff6f4285d5d927
Ahmed Magdy <ahmed.magdy.g.s@gmail.com>
2016-12-17 23:32:04 +0200

    Test OrderBy, Alter

--- DBMS/src/eg/edu/alexu/csd/oop/DBMS/junitTests/JDBCResultSetMetaDataTest.java
+++ DBMS/src/eg/edu/alexu/csd/oop/DBMS/junitTests/JDBCResultSetMetaDataTest.java
@@ -14,61 +14,61 @@ import eg.edu.alexu.csd.oop.DBMS.plugins.jdbc.Driver;
 
 public class JDBCResultSetMetaDataTest {
 
-     @Test
-     public void getColumnCounts() throws SQLException {
-     Driver driver = new Driver();
-     Properties info = new Properties();
-     info.setProperty("path", System.getProperty("user.home"));
-     String url = "jdbc:xmldb://localhost";
-     java.sql.Connection connection = driver.connect(url, info);
-     Statement statement = connection.createStatement();
-     ResultSet temp;
-     statement.execute("create database test");
-     statement.execute("use test");
-     statement.execute("create table tester (name varchar,id int, salary float)");
-     temp = statement.executeQuery("select * from tester");
-     assertEquals(temp.getMetaData().getColumnCount(),3);
-     statement.execute("insert into tester values (\"ahmed\",15, 32.5)");
-     statement.execute("insert into tester values (\"ahmed\",15, 32.5)");
-     statement.execute("alter table tester add lastName varchar");
-     temp = statement.executeQuery("select * from tester");
-     assertEquals(temp.getMetaData().getColumnCount(),4);
-     statement.execute("alter table tester add age int");
-     temp = statement.executeQuery("select * from tester");
-     assertEquals(temp.getMetaData().getColumnCount(),5);
-     statement.execute("alter table tester drop column lastName");
-     temp = statement.executeQuery("select * from tester");
-     assertEquals(temp.getMetaData().getColumnCount(),4);
-     statement.execute("drop database test");
-     }
+    @Test
+    public void getColumnCounts() throws SQLException {
+        Driver driver = new Driver();
+        Properties info = new Properties();
+        info.setProperty("path", System.getProperty("user.home"));
+        String url = "jdbc:xmldb://localhost";
+        java.sql.Connection connection = driver.connect(url, info);
+        Statement statement = connection.createStatement();
+        ResultSet temp;
+        statement.execute("create database test");
+        statement.execute("use test");
+        statement.execute("create table tester (name varchar,id int, salary float)");
+        temp = statement.executeQuery("select * from tester");
+        assertEquals(temp.getMetaData().getColumnCount(), 3);
+        statement.execute("insert into tester values (\"ahmed\",15, 32.5)");
+        statement.execute("insert into tester values (\"ahmed\",15, 32.5)");
+        statement.execute("alter table tester add lastName varchar");
+        temp = statement.executeQuery("select * from tester");
+        assertEquals(temp.getMetaData().getColumnCount(), 4);
+        statement.execute("alter table tester add age int");
+        temp = statement.executeQuery("select * from tester");
+        assertEquals(temp.getMetaData().getColumnCount(), 5);
+        statement.execute("alter table tester drop column lastName");
+        temp = statement.executeQuery("select * from tester");
+        assertEquals(temp.getMetaData().getColumnCount(), 4);
+        statement.execute("drop database test");
+    }
 
-     @Test
-     public void getColumnLabel() throws SQLException {
-     Driver driver = new Driver();
-     Properties info = new Properties();
-     info.setProperty("path", System.getProperty("user.home"));
-     String url = "jdbc:xmldb://localhost";
-     java.sql.Connection connection = driver.connect(url, info);
-     Statement statement = connection.createStatement();
-     ResultSet temp;
-     statement.execute("create database test");
-     statement.execute("use test");
-     statement.execute("create table tester (name varchar,id int, salary float)");
-     temp = statement.executeQuery("select * from tester");
-     assertEquals(temp.getMetaData().getColumnLabel(2),"id");
-     statement.execute("insert into tester values (\"ahmed\",15, 32.5)");
-     statement.execute("insert into tester values (\"ahmed\",15, 32.5)");
-     statement.execute("alter table tester add lastName varchar");
-     temp = statement.executeQuery("select * from tester");
-     assertEquals(temp.getMetaData().getColumnLabel(4),"lastName");
-     statement.execute("alter table tester add age int");
-     temp = statement.executeQuery("select * from tester");
-     assertEquals(temp.getMetaData().getColumnLabel(5),"age");
-     statement.execute("alter table tester drop column lastName");
-     temp = statement.executeQuery("select * from tester");
-     assertEquals(temp.getMetaData().getColumnLabel(4),"age");
-     statement.execute("drop database test");
-     }
+    @Test
+    public void getColumnLabel() throws SQLException {
+        Driver driver = new Driver();
+        Properties info = new Properties();
+        info.setProperty("path", System.getProperty("user.home"));
+        String url = "jdbc:xmldb://localhost";
+        java.sql.Connection connection = driver.connect(url, info);
+        Statement statement = connection.createStatement();
+        ResultSet temp;
+        statement.execute("create database test");
+        statement.execute("use test");
+        statement.execute("create table tester (name varchar,id int, salary float)");
+        temp = statement.executeQuery("select * from tester");
+        assertEquals(temp.getMetaData().getColumnLabel(2), "id");
+        statement.execute("insert into tester values (\"ahmed\",15, 32.5)");
+        statement.execute("insert into tester values (\"ahmed\",15, 32.5)");
+        statement.execute("alter table tester add lastName varchar");
+        temp = statement.executeQuery("select * from tester");
+        assertEquals(temp.getMetaData().getColumnLabel(4), "lastName");
+        statement.execute("alter table tester add age int");
+        temp = statement.executeQuery("select * from tester");
+        assertEquals(temp.getMetaData().getColumnLabel(5), "age");
+        statement.execute("alter table tester drop column lastName");
+        temp = statement.executeQuery("select * from tester");
+        assertEquals(temp.getMetaData().getColumnLabel(4), "age");
+        statement.execute("drop database test");
+    }
 
     @Test
     public void getColumnType() throws SQLException {
