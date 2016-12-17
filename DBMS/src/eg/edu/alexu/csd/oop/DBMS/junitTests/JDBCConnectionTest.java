2786231d1a8e28740d0b37aa2d44fd183864254f
Ahmed Magdy <ahmed.magdy.g.s@gmail.com>
2016-12-17 23:45:58 +0200

    JUnit Tests Done

---
+++ DBMS/src/eg/edu/alexu/csd/oop/DBMS/junitTests/JDBCConnectionTest.java
@@ -0,0 +1,26 @@
+package eg.edu.alexu.csd.oop.DBMS.junitTests;
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+import java.util.Properties;
+
+import org.junit.Test;
+
+import eg.edu.alexu.csd.oop.DBMS.plugins.jdbc.Driver;
+
+public class JDBCConnectionTest {
+
+    @Test(expected = SQLException.class)
+    public void testUsingStatemenThroughClosedConnection() throws SQLException {
+        Driver driver = new Driver();
+        java.sql.Connection connection = driver.connect("jdbc:altdb://localhost", new Properties());
+        java.sql.Statement statement = connection.createStatement();
+        statement.executeQuery("create Database school");
+        statement.executeQuery("use school");
+        statement.executeQuery("create table students(id int, Name varchar, Birthday Date))");
+        statement.executeQuery("insert into students values(1,\"Bill\", 2008-07-27)");
+        connection.close();
+        statement.executeQuery("select * from students");
+    }
+}
